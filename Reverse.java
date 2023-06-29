b9c896821f308c3a77eaf1d5f8c7543fcc3aa89a
abhik <abhikdas0811@gmail.com>
2023-06-29 21:38:49 +0530

    name change

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,7 @@ class Reverse
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Well hello :)");
         System.out.println("Enter the number: ");
         long a= sc.nextLong();
         long n=a;
